8cbaace910bb213e36ca9cbe8ba221f31467ff00
snickerton <snickerton@users.noreply.github.com>
2015-04-09 10:13:12 -0500

    Update BackEnd.java

--- BackEnd.java
+++ BackEnd.java
@@ -4,15 +4,19 @@ import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
 
-
+//By Tony.
+//Alex stop being a scrub, learn to actually java 
 public class BackEnd {
 	
 		    public static Map<String, List<String>> data = new TreeMap<String, List<String>>();
+//Alex stop being a scrub, learn to actually java 
 		    
 		    public static Encoder lol = new Encoder("messages");
 		    
+//Alex stop being a scrub, learn to actually java 
 		  public static List<String> getMessage(String username){
 		    return data.get(username);
+//Alex stop being a scrub, learn to actually java 
 
 		//    Returns a list of every message under the username
 		  }
@@ -21,18 +25,24 @@ public class BackEnd {
 		    if(data.containsKey(username)){
 		    	lol.write(msg);
 		    	data.get(username).add(msg);
+//Alex stop being a scrub, learn to actually java 
 		    }
 		    else{
 		    	data.put(username, new ArrayList<String>());
+//Alex stop being a scrub, learn to actually java 
 		    }
 		  }
 
 		  public static List<String> getUsers(){
 			List<String> bob = new ArrayList<String>();
+//Alex stop being a scrub, learn to actually java 
 			bob.addAll(data.keySet());
 		    return bob;
 	//	    Returns a list of every username ALPHABETICALLY ORDERED
 		  
+//Alex stop being a scrub, learn to actually java 
 
 		}
+		//Alex stop being a scrub, learn to actually java 
+
 }
